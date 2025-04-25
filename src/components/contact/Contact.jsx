93c0168224693db4d508b07fd11680eceff80acb
babi2707 <90732683+babi2707@users.noreply.github.com>
2025-04-25 13:46:37 +0100

    contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,9 +2,57 @@ import React from "react";
 import "./contact.css";
 
 const Contact = () => {
-    return (
-        <div>Contact</div>
-    )
-}
+  return (
+    <section className="contact container section" id="contact">
+      <h2 className="section__title">Get in Touch</h2>
 
-export default Contact
+      <div className="contact__container grid">
+        <div className="contact__info">
+          <h3 className="contact__title">Contact me</h3>
+          <p className="contact__details">Send me an email.</p>
+        </div>
+
+        <form action="" className="contact__form">
+          <div className="contact__form-group">
+            <div className="contact__form-div">
+              <input
+                type="text"
+                className="contact__form-input"
+                placeholder="Insert your name"
+              />
+            </div>
+
+            <div className="contact__form-div">
+              <input
+                type="email"
+                className="contact__form-input"
+                placeholder="Insert your email"
+              />
+            </div>
+
+            <div className="contact__form-div">
+              <input
+                type="text"
+                className="contact__form-input"
+                placeholder="Insert your subject"
+              />
+            </div>
+
+            <div className="contact__form-div contact__form-area">
+              <textarea
+                name=""
+                id=""
+                className="contact__form-input"
+                placeholder="Write your message"
+              ></textarea>
+            </div>
+          </div>
+
+          <button className="btn">Send</button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
